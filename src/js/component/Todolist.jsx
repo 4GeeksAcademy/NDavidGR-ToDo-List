5d2d2bf6e1d04ef004dc555483c910a8f9d2717b
NDavidGR <146215925+NDavidGR@users.noreply.github.com>
2024-02-23 01:54:30 +0000

    ToDo List using React and Fetch

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Todolist = () => {
 
@@ -6,15 +6,69 @@ export const Todolist = () => {
     const [activeIndex, setActiveIndex] = useState(null);
     const [inputValue, setInputValue] = useState('');
     const [tareas, setTareas] = useState([]);
+    const [error, setError] = useState(false);
 
     // Funciones
+
+    let nombreDeUsuario = "NDavidGR"
+    function crearUsuario() {
+        fetch(`https://playground.4geeks.com/apis/fake/todos/user/${nombreDeUsuario}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify([])
+        })
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => {
+                console.log(resultado);
+            })
+    }
+    useEffect(function () {
+        fetch(`https://playground.4geeks.com/apis/fake/todos/user/${nombreDeUsuario}`)
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => {
+                console.log(resultado);
+                if (resultado.msg == "The user NDavid doesn't exists") {
+                    setError(true)
+                    return;
+                }
+                setTareas(resultado)
+            })
+            .catch((error) => {
+                console.log(error);
+                setError(true);
+            })
+
+    }, [])
+
+    useEffect(function () {
+        if (error == true) {
+            crearUsuario()
+            setError(false)
+        }
+        console.log("se creo un nuevo usuario", error);
+    }, [error])
+
+    useEffect(function () {
+        fetch(`https://playground.4geeks.com/apis/fake/todos/user/${nombreDeUsuario}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(tareas)
+        })
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => {
+                console.log(resultado);
+            })
+    }, [tareas])
+
+
+
 
     function handleChange(event) { setInputValue(event.target.value) };
 
     function handleSubmit(event) {
         event.preventDefault()
         if (inputValue.trim() !== "") {
-            setTareas([...tareas, inputValue.trim()])
+            setTareas([...tareas, { label: inputValue.trim(), done: false }])
             setInputValue("")
         }
     };
@@ -41,15 +95,15 @@ export const Todolist = () => {
                 <ul className="col-xs-auto col-md-6 list-group mt-4">
                     {tareas.map((tarea, i) => (
                         <li className="list-group-item d-flex justify-content-between align-items-center text-wrap text-break" key={i} onMouseEnter={(index) => handleSelect(i)} onMouseLeave={() => handleSelect(null)}>
-                            {tarea}
+                            {tarea.label}
                             <button className={`btn btn-close${activeIndex === i ? '' : 'visually-hidden'}`} onClick={() => handleDelete(i)} key={i}></button>
                         </li>
                     ))}
                 </ul>
                 <div className="row d-flex justify-content-center">
-                <div className="col-xs-auto col-md-6 text-end mt-3">{tareas.length} Pending Task</div>
+                    <div className="col-xs-auto col-md-6 text-end mt-3">{tareas.length} Pending Task</div>
                 </div>
-                
+
             </div>
         </div>
     );
